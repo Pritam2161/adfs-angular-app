0a3245e67d9a4816b9e753d04e08142e815e3e6d
pritamshaha <pritam.shaha@zeotap.com>
2021-02-19 10:36:43 +0530

    added iframe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';  
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -16,6 +17,7 @@ const rootRouterConfig: Routes = [
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     RouterModule.forRoot(rootRouterConfig, { useHash: false }),
   ],
   providers: [],
